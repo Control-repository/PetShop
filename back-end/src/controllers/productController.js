90027ad3c9009737afcd22645fc5c78fb4fd508f
TCNGHIA1 <anhlaaikgz12@gmail.com>
2023-07-05 15:21:19 +0700

    design SignIn screen#3

--- back-end/src/controllers/productController.js
+++ back-end/src/controllers/productController.js
@@ -1,5 +1,6 @@
 const myConnection = require("../database/connect.db");
 
+//get All products
 const getAllProducts = (req, res) => {
   const query = "SELECT * from products";
 
@@ -12,9 +13,11 @@ const getAllProducts = (req, res) => {
     return res.json(results);
   });
 };
+
+//insert product
 const insertProduct = (req, res) => {
   const { name, category, price, quantity, description, imageURL } = req.body;
-  console.log(req.body);
+
   if (!name || !category || !price || !quantity) {
     res.status(400).json({ error: "Missing required fields" });
     return;
@@ -28,7 +31,34 @@ const insertProduct = (req, res) => {
       console.log("Failed to insert prodcut", err);
       return res.status(500).json({ error: "Failed to insert prodcut" });
     }
-    return res.json(result);
+    res
+      .status(200)
+      .json({ message: "Product inserted successfully", product: result });
+    console.log(result);
+  });
+};
+
+//update information product
+const updateProduct = (req, res) => {
+  const { id, name, category, price, quantity, description, imageURL } =
+    req.body;
+
+  if (!name || !category || !price || !quantity) {
+    res.status(400).json({ error: "Missing required fields" });
+    return;
+  }
+
+  const query = `UPDATE products SET name='${name}', category='${category}', price=${price}, quantity=${quantity}, description='${description}', imageURL='${imageURL}' WHERE id=${id}`;
+
+  myConnection.query(query, (err, result) => {
+    if (err) {
+      console.log("Failed to insert prodcut", err);
+      return res.status(500).json({ error: "Failed to insert prodcut" });
+    }
+    res
+      .status(200)
+      .json({ message: "Product inserted successfully", product: result });
+    console.log(result);
   });
 };
 
--- font-end/PetShop/app/src/main/java/com/example/petshop/SplashActivity.java
+++ font-end/PetShop/app/src/main/java/com/example/petshop/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.petshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this,LoginActivity.class));
+            finish();
+        },3000);
     }
 }
